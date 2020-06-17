436a986470f70475493f58a0c4be54b041a3f9ee
Bohdan <bohdan.makovetskyi@gmail.com>
2020-06-17 18:51:51 +0300

    Updates

---
+++ precache-manifest.549e371393a8003d8611e8ff827bb6ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e5d8f360ef3057711f08a49ce25fee",
+    "url": "/form_registration/index.html"
+  },
+  {
+    "revision": "3efe6ad6771d62c7ce9f",
+    "url": "/form_registration/static/css/main.5125e45c.chunk.css"
+  },
+  {
+    "revision": "9934b58e2a3f0de07d98",
+    "url": "/form_registration/static/js/2.34c22338.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form_registration/static/js/2.34c22338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3efe6ad6771d62c7ce9f",
+    "url": "/form_registration/static/js/main.ea75107a.chunk.js"
+  },
+  {
+    "revision": "8f7c5b55c4e0f60ca0ce",
+    "url": "/form_registration/static/js/runtime-main.544a1feb.js"
+  }
+]);
